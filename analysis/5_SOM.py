a90750f655368da124b68033f45f3d6067448517
shadyagamal <shadya.gamal@gmail.com>
2025-03-17 18:14:37 +0100

    end of day commit

--- analysis/5_SOM.py
+++ analysis/5_SOM.py
@@ -19,7 +19,23 @@ import umap
 import somoclu
 
 #%% Step 1: Define the paths
-figs_path = Path("~/gpm_storm/figs")
-Data_path = Path("~/gpm_storm/data/")
+figs_path = Path("~/figs")
+Data_path = Path("~data/")
+
+# %%
+# Initialize lists to store image data
+image_data = []
+image_filenames = []
+
+# Iterate through the files in the directory
+for filename in os.listdir(figs_path):
+    if filename.endswith(".png"):
+        # Open the image and convert it to a NumPy array
+        image = Image.open(os.path.join(figs_path, filename))
+        image_array = np.array(image)
+
+        # Append the image data and filename to lists
+        image_data.append(image_array)
+        image_filenames.append(filename)
 
 # %%
